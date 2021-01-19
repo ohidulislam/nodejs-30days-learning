dd59de34802bf4862733499b5e6e341131eecd36
Md Ohidul Islam <ohiduli69@gmail.com>
2021-01-19 23:16:24 +0600

    day_4 update_4

--- Day_4/express-demo/index.js
+++ Day_4/express-demo/index.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const app = express()
 
-// console.log(express());
-// console.log(typeof app);
+const courses = [
+    {id: 1, name: 'Course 1', price: '$9.92'},
+    {id: 2, name: 'Course 2', price: '$3.35'},
+    {id: 3, name: 'Course 3', price: '$6.80'}
+]
 
 app.get('/', (req, res) => {
     res.send("hello world!!!!")
 })
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4, 5])
+    res.send(courses)
 })
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params)
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find( c => c.id === parseInt(req.params.id) )
+    if(!course) res.status(404)
+    res.send(course)
 })
 
 const port = process.env.PORT || 3000
